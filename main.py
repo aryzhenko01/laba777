1a5f71e78e1d7ab45068387aac5d57a7d4bfc177
aryzhenko01 <a.ryzhenko01@mail.ru>
2025-12-03 01:00:49 +0300

    1ода

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+def check(i):
+    while True:
+        try:
+            b = int(input(i))
+            if b > 0:
+                return b
+            else:
+                print('Введенное число некорректно. Число должно быть больше 0.')
+        except ValueError:
+            print('Вы ввели не число')
+
+f = check('Введите количество строк списка: ')
+t = check('Введите количество элементов строки: ')
+print(f"Размер матрицы: {f}x{t}")
+
+
+matr=[]
+for i in range(f):
+    elem=[]
+    for j in range(t):
+        elem.append(random.randint(0,100))
+    matr.append(elem)
+print('Полученный список ',matr)
+
+for i in range(len(matr)):
+    sum=[]
+    for j in range(len(matr[i])):
+        if matr[i][j]%2==0 and matr[i][j]>0:
+            sum.append(matr[i][j])
+
+v=[]
+for i in range(len(sum)):
+    for j in range(i+1,len(sum)):
+        if sum[i] < sum[j]:
+            sum[i], sum[j] = sum[j], sum[i]
+            matr[i], matr[j] =  matr[j], matr[i]
+print(sum)
+print(matr)
+
